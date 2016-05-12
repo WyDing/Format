fa52b6ae178b39516d3fa72fecee57b4af5a2427
WyDing <15811568903@163.com>
2016-05-12 15:26:13 +0800

    添加金额格式化方法

--- formatter/src/androidTest/java/com/duanding/formatter/ApplicationTest.java
+++ formatter/src/androidTest/java/com/duanding/formatter/ApplicationTest.java
@@ -11,8 +11,13 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testInsertionFormatter(){
-        InsertionFormatter inf = new InsertionFormatter(" ","399998");
-        assertEquals("123 456", inf.format("123456"));
+//    public void testInsertionFormatter(){
+//        InsertionFormatter inf = new InsertionFormatter(" ","399998");
+//        assertEquals("123 456", inf.format("123456"));
+//    }
+
+    public void testMoneyFormatter(){
+        MoneyFormatter mf = new MoneyFormatter();
+        assertEquals("112,123,122,313",mf.format("112123122313"));
     }
 }
--- formatter/src/main/java/com/duanding/formatter/MoneyFormatter.java
+++ formatter/src/main/java/com/duanding/formatter/MoneyFormatter.java
@@ -5,10 +5,32 @@ package com.duanding.formatter;
  */
 public class MoneyFormatter implements IFormat {
 
+    public static final int COUNT_NODE = 3;
+    private int countLen;
     private InsertionFormatter insertionFormatter;
-
+    public MoneyFormatter(){
+        insertionFormatter = new InsertionFormatter();
+        insertionFormatter.setInsertStr(",");
+        countLen = COUNT_NODE;
+    }
     @Override
     public String format(String str) {
-        return "";
+        int count = str.length()/countLen;
+        int [] counts = new int[count + 1];
+        int first = str.length() - count * countLen;
+        if(0 == first){
+            counts[0] = 3;
+        }else {
+            counts[0] = str.length() - count * countLen;
+        }
+        for (int i = 0; i < count; i ++){
+            counts[i + 1] = countLen;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < counts.length; i ++){
+            sb.append(counts[i]);
+        }
+        insertionFormatter.setNodeStr(sb.toString());
+        return insertionFormatter.format(str);
     }
 }
